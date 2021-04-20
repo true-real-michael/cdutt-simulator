868a99546b9e5ef1c235f030bc30d8d5e12117ff
true-real-michael <64229743+true-real-michael@users.noreply.github.com>
2021-04-20 18:00:21 +0300

    Third commit

--- simulator.py
+++ simulator.py
@@ -1,20 +1,6 @@
 import wexpect
 
-
-# ---------------------- CONSTANTS ---------------------- #
-"""
-MOVE_LIMIT    - преел ходов, по правилам игры - 150
-TEAM_X_FILE   - путь к файлу команды X
-HOUSE PATTERN - расстановка домов 
-TEAM_X_MANUAL - если True, ввод и вывод для этой команды будет перенаправлен в консоль 
-"""
-
-MOVE_LIMIT = 150
-TEAM_0_FILE = 'team0.exe'
-TEAM_1_FILE = 'team1.exe'
-HOUSE_PATTERN = 'F4 G2 G8 H3 H6 I1 I9 J5 K3 K7 L1 L6 L9'
-TEAM_O_MANUAL = False
-TEAM_1_MANUAL = False
+from config import *
 
 # ------------------------- MAP ------------------------- #
 
